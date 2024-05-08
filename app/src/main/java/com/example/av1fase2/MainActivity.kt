4bb6c9c8eddf4f1209b53f61ca87d9794179f10a
Gabriel Henryque <gabriel.henryque@unemat.br>
2024-05-08 19:12:44 -0400

    iniciando xml

--- app/src/main/java/com/example/av1fase2/MainActivity.kt
+++ app/src/main/java/com/example/av1fase2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.av1fase2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val spinner = findViewById<Spinner>(R.id.campus)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_options, android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.setAdapter(adapter)
+
+        val datePicker = findViewById<DatePicker>(R.id.data)
     }
 }
