084eecde3523ddfdf2257b5770d74d2c03552adb
Gabriel Henryque <gabriel.henryque@unemat.br>
2024-05-09 17:14:50 -0400

    ajustando mais algumas coisas

--- app/src/main/java/com/example/av1fase2/MainActivity.kt
+++ app/src/main/java/com/example/av1fase2/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.av1fase2
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.DatePicker
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +33,34 @@ class MainActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.setAdapter(adapter)
 
-        val datePicker = findViewById<DatePicker>(R.id.data)
+        val datePicker = findViewById<DatePicker>(R.id.pegarData)
+        val buttonDatePicker = findViewById<Button>(R.id.btnData)
+        buttonDatePicker.setOnClickListener {
+            showDatePickerDialog()
+        }
+
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun showDatePickerDialog() {
+        val calendario = Calendar.getInstance()
+        val ano = calendario.get(Calendar.YEAR)
+        val mes = calendario.get(Calendar.MONTH)
+        val dia = calendario.get(Calendar.DAY_OF_MONTH)
+        val dataSelecionada = findViewById<TextView>(R.id.dataSelecionada)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val data = "${selectedDayOfMonth}/${selectedMonth + 1}/${selectedYear}"
+                dataSelecionada.text = "Data Selecionada: ${data}"
+            },
+            ano,
+            mes,
+            dia
+        )
+
+        datePickerDialog.show()
+    }
+
 }
